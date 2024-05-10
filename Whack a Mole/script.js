672d16a98eb28a121061a2d6e9af3d4f5d384654
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-05-10 15:04:39 -0230

    Adds ninth project: Whack a Mole Game

---
+++ Whack a Mole/script.js
@@ -0,0 +1,46 @@
+// Variables
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+// Functions
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  return hole === lastHole ? randomHole(holes) : (lastHole = hole);
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+function whack(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+// Event Listeners
+moles.forEach((mole) => mole.addEventListener("click", whack));
